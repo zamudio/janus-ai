01ec66a95c42014fc19f8dfab12df7f887b9d59e
github username <github email address>
2020-05-17 02:58:24 -0700

    initial file setup

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./container/App";
+// import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
