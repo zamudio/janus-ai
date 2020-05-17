ebf71b88f58bbd6bf54f216e55ef6ab2317aa330
github username <github email address>
2020-05-17 05:51:52 -0700

    added clarifai api

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -39,10 +39,12 @@ class App extends Component {
 
   onBtnSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    app.models.predict(Clarifai.COLOR_MODEL, this.state.input).then(
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
       function (response) {
         // do something with response
-        console.log(response);
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        );
       },
       function (err) {
         // there was an error
