0d44678df5fb569604b7f6d834cb1aec8876ebce
github username <github email address>
2020-05-22 04:02:43 -0700

    style change user.jsx

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const User = ({ name, entries }) => {
   return (
     <div>
-      <div className="f3 pa5">{`Hello, ${name}! The # of photos that you've submitted so far is... ${entries}`}</div>
+      <div className="f3 pa5">
+        {`Hello, ${name}!`}
+        {`The # of photos that you've submitted so far is... ${entries}`}
+      </div>
     </div>
   );
 };
